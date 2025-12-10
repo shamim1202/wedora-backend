47d62224f3b5f3961adc8a4b429148ca0ea517fc
shamim1202 <mehedi.farzu@gmail.com>
2025-12-10 12:36:28 +0600

    create get api to get all services and specific id wise service

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qx9mzcm.mongodb.net/?appName=Cluster0`;
@@ -24,9 +24,39 @@ async function run() {
     const db = client.db("wedoraDB");
     const usersCollection = db.collection("users");
     const servicesCollection = db.collection("services");
+    const bookingsCollection = db.collection("bookings");
 
-    //===>====>=====>====>===> 
-    //===>====>=====>====>===> Added New User In The Database 
+    //===>====>=====>====>===> Get All Services Api
+    app.get("/services", async (req, res) => {
+      const cursor = servicesCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //===>====>=====>====>===> Get Popular Services Api
+    app.get("/top-services", async (req, res) => {
+      const cursor = servicesCollection.find().limit(4);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //===>====>=====>====>===> Get Specific Service(id) Api
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await servicesCollection.findOne(query);
+      res.send(result);
+    });
+
+    //===>====>=====>====>===> Get All Users From Database Api
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const cursor = usersCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //===>====>=====>====>===> Added New User In The Database
     app.post("/users", async (req, res) => {
       const user = req.body;
       const exists = await usersCollection.findOne({ email: user.email });
@@ -34,6 +64,21 @@ async function run() {
         return res.send({ message: "User already exists" });
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //===>====>=====> Store Booking Service In The Database Api
+    app.post("/bookings", async (req, res) => {
+      const booking = req.body;
+      const result = await bookingsCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    //===>====>=====>====> Get Booking Service From The Database Api
+    app.get("/bookings", async (req, res) => {
+      const query = {};
+      const cursor = bookingsCollection.find(query);
+      const result = await cursor.toArray();
       res.send(result);
     });
 
